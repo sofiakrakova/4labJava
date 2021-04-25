31fd77cf53e31f5074957ef3c5e034c0730958db
sofiakrakova <sofiakrakova84@gmail.com>
2021-04-25 20:13:16 +0300

    Create Department.java

---
+++ src/main/java/Department.java
@@ -0,0 +1,13 @@
+public class Department {
+    private long id;
+    private String name;
+
+    public Department(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
